7246520b898dddb038298a4769ff8f8df0907cbb
igdaloff <louis.nathan@gmail.com>
2013-06-18 22:36:40 -0600

    Updating cap dev file to reflect correct dev folder

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,6 +1,6 @@
 set :branch, "develop"
 server 'ec2-54-218-36-67.us-west-2.compute.amazonaws.com', :app, :web, :db, :primary => true
-set(:deploy_to) { '/dev' }
+set(:deploy_to) { '/develop' }
 
 set :user, "ec2-user"
 ssh_options[:keys] = %w(~/.ssh/igdaloff-server.pem)
