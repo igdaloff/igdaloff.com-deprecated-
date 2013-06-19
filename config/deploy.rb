39cb853c7b6411e8b0ba0bcd3cdba61742bcc2a6
igdaloff <louis.nathan@gmail.com>
2013-06-18 21:41:18 -0600

    Updating cap deploy info

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,19 @@ set :default_stage, "dev"
 require 'capistrano/ext/multistage'
 
 set :application, "igdaloff_site"
+
 set :scm, :git
 set :repository, "git@github.com:igdaloff/igdaloff.com.git"
 set :scm_passphrase, ""
 
-set :user, "ec2-user"
-ssh_options[:keys] = %w(~/.ssh/igdaloff-server.pem)
+set :user, "ubuntu"
+set :deploy_to, '/home/ubuntu/'
+
+server "54.218.28.224", :app, :primary => true
 
 ssh_options[:forward_agent] = true
-default_run_options[:pty] = true
+ssh_options[:auth_methods] = ["publickey"]
+ssh_options[:keys] = ["~/.ssh/igdaloff-server.pem"]
+
+# default_run_options[:pty] = true
+
