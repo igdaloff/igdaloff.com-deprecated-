40cd9937a408f91583b3a78068c10cd7ae4d8d3b
igdaloff <louis.nathan@gmail.com>
2013-06-30 19:21:09 -0600

    Adding header link anayltics and updating cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,10 @@ set :repository, "git@github.com:igdaloff/igdaloff.com.git"
 set :scm_passphrase, ""
 
 set :user, "ubuntu"
-#set :deploy_to, '/home/ubuntu/'
 
-server "54.218.28.224", :app, :primary => true
+server "54.218.28.224", :app, :web, :primary => true
 
+default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:auth_methods] = ["publickey"]
 ssh_options[:keys] = ["~/.ssh/igdaloff-server.pem"]
